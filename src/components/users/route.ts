feb78cc6a141e2fe6499ac2549f0ecf32aa221c1
julefikar <julfikar.chowdhury50@myhunter.cuny.edu>
2023-01-18 02:23:05 -0500

    LoginUser routing

--- src/components/users/route.ts
+++ src/components/users/route.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import { reqValidation } from '../../middleware/reqValidation';
-import { registerUser } from './controller';
-import { registerSchema } from './joiSchema';
+import { loginUser, registerUser } from './controller';
+import { loginSchema, registerSchema } from './joiSchema';
 
 const router = express.Router();
 
 //create another route for login
 //replace register user
 router.post('/register', reqValidation(registerSchema), registerUser);
+router.post('/login',reqValidation(loginSchema),loginUser);
 export default router;
