de3872a82a1573e753a6ed6e1db8809e13473f05
Anisul <anisulhaque9391@gmail.com>
2023-01-20 20:35:19 -0500

    BUILD: enabled cors and added client as allowed origin

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import router from './components/users/route';
 import dbConnection from './config/dbConnection';
 import httpLogger from './middleware/httpLogger';
 import routeError from './middleware/routeError';
+import cors, { CorsOptions } from 'cors';
 import './process';
 
 const app: Application = express();
@@ -10,6 +11,13 @@ const app: Application = express();
 
 dbConnection();
 app.use(httpLogger)
+const allowedOrigins = ['http://localhost:5173'];
+
+const options: CorsOptions = {
+  origin: allowedOrigins
+};
+
+app.use(cors(options))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
