6db5e44829cd21f7b7197d8101962a5a1f5f05e6
Anisul <anisulhaque9391@gmail.com>
2023-01-21 00:23:57 -0500

    FIX: updated regex for password

--- src/components/users/joiSchema.ts
+++ src/components/users/joiSchema.ts
@@ -4,7 +4,7 @@ export const registerSchema = Joi.object({
     firstName: Joi.string().alphanum().min(3).max(30).required(),
     lastName: Joi.string().alphanum().min(3).max(30).required(),
     password: Joi.string()
-        .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
+        .pattern(new RegExp(/^(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{5,}$/))
         .required(),
     confirmPassword: Joi.ref('password'),
     email: Joi.string()
