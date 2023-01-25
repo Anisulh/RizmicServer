62eaabfbda4ca4ae995eeed6b5fd46b4cf742cb5
Anisul <anisulhaque9391@gmail.com>
2023-01-25 14:07:00 -0500

    FEAT: implemented new rate limiting middleware

---
+++ src/middleware/rateLimiter.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from "express";
+import { rateLimiter } from "../server";
+
+
+
+
+const rateLimiterMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  rateLimiter
+      .consume(req.ip)
+      .then(() => {
+          next();
+      })
+      .catch((_) => {
+          res.status(429).send('Too Many Requests');
+      });
+};
+
+export default rateLimiterMiddleware
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,23 @@
-import express, { Application } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 import router from './components/users/route';
 import dbConnection from './config/dbConnection';
 import httpLogger from './middleware/httpLogger';
 import routeError from './middleware/routeError';
 import cors, { CorsOptions } from 'cors';
+import Redis from 'ioredis';
+import { RateLimiterRedis } from 'rate-limiter-flexible';
 import './process';
+import rateLimiterMiddleware from './middleware/rateLimiter';
 
+export const redis = new Redis();
+export const rateLimiter = new RateLimiterRedis({
+    storeClient: redis,
+    keyPrefix: 'middleware',
+    points: 10, // 10 requests
+    duration: 1 // per 1 second by IP
+  });
+
+  
 const app: Application = express();
 
 dbConnection();
@@ -17,6 +29,10 @@ const options: CorsOptions = {
 };
 
 app.use(cors(options));
+
+
+app.use(rateLimiterMiddleware);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
