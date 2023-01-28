c7f3b0660f41946d075ca64d1ae6da68e1bf5be7
Anisul <anisulhaque9391@gmail.com>
2023-01-28 17:52:48 -0500

    FEAT: added validation middleware to respective routes

--- src/components/clothes/route.ts
+++ src/components/clothes/route.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { authorization } from '../../middleware/authorization';
+import { reqValidation } from '../../middleware/reqValidation';
 import {
     createClothes,
     deleteClothes,
@@ -7,13 +8,19 @@ import {
     getSpecificClothes,
     updateClothes
 } from './controller';
+import { createClothesSchema, updateClothesSchema } from './joiSchema';
 const clothesRouter = express.Router();
 
 clothesRouter
     .get('/', authorization, getAllClothes)
-    .post('/', authorization, createClothes)
+    .post('/', authorization, reqValidation(createClothesSchema), createClothes)
     .get('/:clothesId', authorization, getSpecificClothes)
-    .put('/:clothesId', authorization, updateClothes)
+    .put(
+        '/:clothesId',
+        authorization,
+        reqValidation(updateClothesSchema),
+        updateClothes
+    )
     .delete('/:clothesId', authorization, deleteClothes);
 
 export default clothesRouter;
