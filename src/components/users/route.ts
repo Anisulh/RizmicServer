55cc7ac33a9df34e252591c8341e248f044bdf0a
julefikar <julfikar.chowdhury50@myhunter.cuny.edu>
2023-01-28 15:16:25 -0500

    FEAT: Added routes for controller functions

--- src/components/users/route.ts
+++ src/components/users/route.ts
@@ -1,12 +1,18 @@
 import express from 'express';
 import { reqValidation } from '../../middleware/reqValidation';
-import { loginUser, registerUser } from './controller';
+import {
+    loginUser,
+    registerUser,
+    forgotUserPassword,
+    resetPasswordController
+} from './controller';
 import { loginSchema, registerSchema } from './joiSchema';
 
 const router = express.Router();
 
 router.post('/register', reqValidation(registerSchema), registerUser);
 router.post('/login', reqValidation(loginSchema), loginUser);
-//router.post('/forgotpassword');
+router.post('/forgotpassword', forgotUserPassword);
+router.post('/passwordReset', resetPasswordController);
 
 export default router;
