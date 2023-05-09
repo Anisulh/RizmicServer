d83db67dd33327dbd5dea8732e2f9bb559f976e0
Anisul <anisulhaque9391@gmail.com>
2023-05-09 09:29:33 -0400

    FIX: added better redis error handling

--- src/library/limiterInstances.ts
+++ src/library/limiterInstances.ts
@@ -2,13 +2,15 @@ import { Redis } from 'ioredis';
 import { RateLimiterRedis } from 'rate-limiter-flexible';
 import config from '../config/config';
 
-export const redis = new Redis();
+export const redis = new Redis({showFriendlyErrorStack:true});
+
 export const rateLimiter = new RateLimiterRedis({
     storeClient: redis,
     keyPrefix: 'middleware',
     points: 10, // 10 requests
     duration: 1 // per 1 second by IP
 });
+
 export const limiterSlowBruteByIP = new RateLimiterRedis({
     storeClient: redis,
     keyPrefix: 'login_fail_ip_per_day',
@@ -16,7 +18,7 @@ export const limiterSlowBruteByIP = new RateLimiterRedis({
     duration: 60 * 60 * 24,
     blockDuration: 60 * 60 * 24 // Block for 1 day, if 100 wrong attempts per day
 });
-
+ 
 export const limiterConsecutiveFailsByEmailAndIP = new RateLimiterRedis({
     storeClient: redis,
     keyPrefix: 'login_fail_consecutive_Email_and_ip',
