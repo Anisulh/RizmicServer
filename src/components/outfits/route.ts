e6f7d7d8e5e99a5d8ae8d1a947c529151d817d48
Anisul <anisulhaque9391@gmail.com>
2023-06-25 10:53:53 -0400

    FEAT: added image upload functionality

--- src/components/outfits/route.ts
+++ src/components/outfits/route.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { authorization } from '../../middleware/authorization';
 import {
     createOutfit,
+    deleteOutfit,
     favoriteOutfit,
     listFavoriteOutfits,
     listOutfits,
@@ -9,12 +10,20 @@ import {
 } from './controllers';
 import { createOutfitsSchema } from './joiSchema';
 import { reqValidation } from '../../middleware/reqValidation';
+import upload from '../../config/multer.config';
 
 const outfitRouter = express.Router();
 
 outfitRouter
     .get('/', authorization, listOutfits)
-    .post('/', authorization, reqValidation(createOutfitsSchema), createOutfit)
+    .post(
+        '/',
+        authorization,
+        upload.single('coverImg'),
+        reqValidation(createOutfitsSchema),
+        createOutfit
+    )
+    .delete('/:outfitID', authorization, deleteOutfit)
     .get('/favorite', authorization, listFavoriteOutfits)
     .patch('/favorite/:outfitID', authorization, favoriteOutfit)
     .patch('/unfavorite/:outfitID', authorization, unfavoriteOutfit);
