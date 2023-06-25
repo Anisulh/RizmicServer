8a390cec15b18dc85b74fd6baa87395e6e7ce0ad
Anisul <anisulhaque9391@gmail.com>
2023-06-25 10:58:26 -0400

    REFACTOR: removed logs and added vibe param

--- src/components/fitGeneration/util.ts
+++ src/components/fitGeneration/util.ts
@@ -2,13 +2,15 @@ import { AnyObject } from 'mongoose';
 import { IClothingData } from '../clothes/models';
 import Color from 'color';
 
-const monochromeThreshold = 30;
+const MONOCHROME_THRESHOLD = 30;
 
+// main function that handles the generation, returns an 2D array of clothes
 export const algorithm = (
     tops: AnyObject,
     bottoms: AnyObject,
-    style: string
-) => {
+    style: string,
+    vibe: string
+): IClothingData[][] => {
     try {
         const fits: IClothingData[][] = [];
         for (let i = 0; i < tops.length; i++) {
@@ -18,7 +20,6 @@ export const algorithm = (
                     bottoms[j],
                     style
                 );
-                console.log(tops[i].color, bottoms[i].color, isSimilar);
                 if (isSimilar) {
                     const matchedFit = [tops[i]._id, bottoms[j]._id];
                     fits.push(matchedFit);
@@ -37,10 +38,10 @@ const checkColorSimilarity = (
     style: string
 ): boolean | undefined => {
     try {
-        console.log(top.color, bottom.color);
         const topLAB: any = Color(top.color).rgb().lab();
         const bottomLAB: any = Color(bottom.color).rgb().lab();
         let distance;
+        //add more styles here
         switch (style) {
             case 'monochrome':
                 distance = Math.sqrt(
@@ -48,8 +49,7 @@ const checkColorSimilarity = (
                         Math.pow(bottomLAB.color[1] - topLAB.color[1], 2) +
                         Math.pow(bottomLAB.color[2] - topLAB.color[2], 2)
                 );
-                console.log(distance);
-                return distance <= monochromeThreshold;
+                return distance <= MONOCHROME_THRESHOLD;
             case 'complimentary':
                 const rgb = Color(top.color).rgb().array();
                 const invertedRgb = rgb.map((channel: number) => 255 - channel);
@@ -59,7 +59,7 @@ const checkColorSimilarity = (
                         Math.pow(bottomLAB[1] - complimentaryLab[1], 2) +
                         Math.pow(bottomLAB[2] - complimentaryLab[2], 2)
                 );
-                return distance <= monochromeThreshold;
+                return distance <= MONOCHROME_THRESHOLD;
             default:
                 break;
         }
