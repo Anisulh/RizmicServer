439267f8cf9cb613f467a014abc5f5963aed9cc6
Anisul <anisulhaque9391@gmail.com>
2023-10-22 20:34:29 -0400

    REFACTOR: factored out the functions for prod/dev

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,33 @@
 import { createHttpTerminator } from 'http-terminator';
-import serverlessExpress from 'aws-serverless-express';
+import { createServer, proxy } from 'aws-serverless-express';
 import config from './config/config';
 import { injectExithandlerDependancy } from './library/exitHandler';
 import logger from './library/logger';
-import app from './server';
 import { APIGatewayProxyEvent, Context } from 'aws-lambda';
+import { Application } from 'express';
+import { initializeServer } from './server';
 
+const awsHandler = async (event: APIGatewayProxyEvent, context: Context) => {
+    const app: Application | null = await initializeServer();
+    const server = createServer(app);
+    context.callbackWaitsForEmptyEventLoop = false;
+    console.log(`EVENT: ${JSON.stringify(event)}`);
+    proxy(server, event, context);
+};
 
-
-if (config.env === 'production') {
-    // Production mode, running on AWS Lambda
-    const server = serverlessExpress.createServer(app);
-    exports.handler = (event: APIGatewayProxyEvent, context: Context) => {
-      context.callbackWaitsForEmptyEventLoop = false;
-      console.log(`EVENT: ${JSON.stringify(event)}`);
-      serverlessExpress.proxy(server, event, context);
-    };
-  } else {
-    // Development mode, running locally
+const startServer = async () => {
+    const app = await initializeServer();
     const server = app.listen(config.port, () => {
-      logger.info(`Server is running on port: ${config.port}`);
+        logger.info(`Server is running on port: ${config.port}`);
     });
     const httpTerminator = createHttpTerminator({ server });
     injectExithandlerDependancy(server, httpTerminator);
-  }
+};
+
+if (config.env === 'production') {
+    // Production mode, running on AWS Lambda
+    exports.handler = awsHandler;
+} else {
+    // Development mode, running locally
+    startServer();
+}
