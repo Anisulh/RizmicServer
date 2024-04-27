b4fbb179371e03c98e873c4d140dad90f5ea4a38
ubergoose <anisulhaque9391@gmail.com>
2024-04-27 03:30:54 -0400

    Refactor request validation middleware to use Zod schemas

--- src/middleware/reqValidation.ts
+++ src/middleware/reqValidation.ts
@@ -1,45 +1,14 @@
 import { NextFunction, Request, Response } from 'express';
-import { ObjectSchema, ValidationError } from 'joi';
-import { AppError, errorHandler, HttpCode } from '../library/errorHandler';
-import logger from '../library/logger';
-import { verifyGoogleToken } from '../components/users/services/googleAuth';
+import { ZodSchema } from 'zod';  
 
-export const reqValidation = (schema: ObjectSchema) => {
-    return async (req: Request, res: Response, next: NextFunction) => {
+export const reqValidation = (schema: ZodSchema<any>) => {
+    return (req: Request, res: Response, next: NextFunction) => {
         try {
-            const token: string | null =
-                req.headers['authorization'] &&
-                req.headers['authorization'].split(' ')[0] === 'Bearer'
-                    ? req.headers['authorization'].split(' ')[1]
-                    : null;
-
-            const googleToken =
-                (token && (await verifyGoogleToken(token))) || false;
-            if (googleToken) {
-                next();
-            } else {
-                await schema.validateAsync(req.body);
-                next();
-            }
-        } catch (error: ValidationError | unknown) {
-            if (error instanceof ValidationError) {
-                logger.error(error.annotate());
-                errorHandler.handleError(
-                    new AppError({
-                        name: 'JOI validation Error',
-                        httpCode: HttpCode.BAD_REQUEST,
-                        description:
-                            'One or more fields submitted was not valid'
-                    }),
-                    req,
-                    res
-                );
-            } else {
-                const criticalError = new Error(
-                    `Unknown error occured in reqValidation: ${error}`
-                );
-                errorHandler.handleError(criticalError, req, res);
-            }
+            schema.parse(req.body);
+            next();
+        } catch (error) {
+           next(error)
         }
     };
 };
+
