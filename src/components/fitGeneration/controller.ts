57f7c888d2afebb312ab0c1b5ef4ab448d656eab
ubergoose <anisulhaque9391@gmail.com>
2024-04-27 03:47:37 -0400

    Refactor fitGeneration controller to remove try catch and update import paths

--- src/components/fitGeneration/controller.ts
+++ src/components/fitGeneration/controller.ts
@@ -1,28 +1,20 @@
 import { Request, Response } from 'express';
 import Clothes from '../clothes/models';
 import { algorithm } from './util';
-import { errorHandler } from '../../library/errorHandler';
 
 export const generateFit = async (req: Request, res: Response) => {
-    try {
-        const { _id } = req.user;
-        const { style, vibe } = req.body;
-        const tops = await Clothes.find({
-            bodyLocation: 'upperBody',
-            userID: _id
-        });
-        const bottoms = await Clothes.find({
-            bodyLocation: 'lowerBody',
-            userID: _id
-        });
+    const { _id } = req.user;
+    const { style, vibe } = req.body;
+    const tops = await Clothes.find({
+        bodyLocation: 'upperBody',
+        userID: _id
+    });
+    const bottoms = await Clothes.find({
+        bodyLocation: 'lowerBody',
+        userID: _id
+    });
 
-        const fits = algorithm(tops, bottoms, style, vibe);
-        console.log('SUCCESSFUL FIT:', fits);
-        res.status(200).json({ fits });
-    } catch (e) {
-        const criticalError = new Error(
-            `Error occured during fit generation: ${e}`
-        );
-        errorHandler.handleError(criticalError, req, res);
-    }
+    const fits = algorithm(tops, bottoms, style, vibe);
+    console.log('SUCCESSFUL FIT:', fits);
+    res.status(200).json({ fits });
 };
