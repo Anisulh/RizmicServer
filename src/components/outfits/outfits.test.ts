9260a07d2281928fd384a20144033d5e74553f16
ubergoose <anisulhaque9391@gmail.com>
2024-04-27 12:09:01 -0400

    Add additional tests to ensure complete coverage

--- src/components/outfits/outfits.test.ts
+++ src/components/outfits/outfits.test.ts
@@ -1,12 +1,12 @@
 import User from '../users/model';
-import bcrypt from 'bcrypt';
-import { AnyObject, Types } from 'mongoose';
+import { Types } from 'mongoose';
 import { generateToken } from '../users/utils/jwt';
 import request from 'supertest';
 import { startApp } from '../../app';
 import Clothes from '../clothes/models';
-import Outifts from './models';
-import { Application } from 'express';
+import Outfits from './models';
+import dbConnection from '../../config/dbConnection';
+import { ClothesInput } from '../clothes/validationSchema';
 
 let token: string | undefined;
 let userID: Types.ObjectId;
@@ -20,93 +20,95 @@ const existingUser = {
     password: '1234567aA'
 };
 
-const existingUpperBodyClothes = [
+const existingClothes: ClothesInput[] = [
     {
-        bodyLocation: ['upperBody'],
+        name: 'Mock-neck sweater',
         category: 'sweater',
-        variant: 'Mock-neck',
-        color: 'rgb(50,205,50)',
-        layerable: true
+        condition: 'new',
+        size: 'm',
+        color: 'black',
+        favorited: false
     },
     {
-        bodyLocation: ['upperBody'],
-        category: 'tshirt',
-        variant: 'crew-neck',
-        color: 'rgb(155, 102, 112)',
-        layerable: true
-    }
-];
-
-const existingLowerBodyClothes = [
-    {
-        bodyLocation: ['lowerBody'],
-        category: 'shorts',
-        variant: 'chino',
-        color: 'rgb(127,255,0)',
-        layerable: true
-    },
-    {
-        bodyLocation: ['lowerBody'],
+        name: 'Jeans',
         category: 'pants',
-        variant: 'jeans',
-        color: 'rgb(155, 92, 102)',
-        layerable: true
+        condition: 'new',
+        size: 'm',
+        color: 'blue',
+        favorited: false
     }
 ];
 
-const app = startApp();
+let app: any;
+
+const startTest = async () => {
+    await dbConnection();
+    app = await startApp(); // Ensure startApp is correctly setting up the Express app
+    return app;
+};
 
 beforeAll(async () => {
-    await Outifts.deleteMany();
+    await startTest();
+    await Outfits.deleteMany();
     await Clothes.deleteMany();
-    const newUser = { ...existingUser };
+    const newUser = new User(existingUser);
     const userInDB = await User.findOne({ email: existingUser.email }).lean();
     if (!userInDB) {
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(existingUser.password, salt);
-        newUser.password = hashedPassword;
-        const createdUser: AnyObject = await User.create(existingUser);
-        if (createdUser) {
-            const createdUserData = { ...createdUser._doc };
-            token = generateToken(createdUserData._id) as string;
-            userID = createdUserData._id;
-        }
+        const createdUser = await newUser.save();
+        token = generateToken(createdUser._id);
+        userID = createdUser._id;
     } else {
         userID = userInDB._id;
-        token = generateToken(userInDB._id) as string;
+        token = generateToken(userInDB._id);
     }
-    const upperBodyExisting = existingUpperBodyClothes.map(async (item) => {
-        try {
-            const clothesData = { ...item, userID };
-            const createdClothes = await Clothes.create(clothesData);
-            return createdClothes._id;
-        } catch (error) {
-            console.log(error);
-        }
+    const clothesPromises = existingClothes.map(async (clothes) => {
+        const newClothes = new Clothes({ ...clothes, userID });
+        const createdClothes = await newClothes.save();
+        return createdClothes._id;
     });
-    const lowerBodyExisting = existingLowerBodyClothes.map(async (item) => {
-        try {
-            const clothesData = { ...item, userID };
-            const createdClothes = await Clothes.create(clothesData);
-            return createdClothes._id;
-        } catch (error) {
-            console.log(error);
-        }
-    });
-    clothesArray = await Promise.all([
-        ...upperBodyExisting,
-        ...lowerBodyExisting
-    ]);
+    clothesArray = await Promise.all(clothesPromises);
 });
 
+const nonExistingOutfit = {
+    name: 'Outfit1',
+    clothes: clothesArray
+};
+let createdOutfitIdWithImage: Types.ObjectId;
+
 describe('create an outfit', () => {
-    it('Should return 200 and all generated instances', async () => {
+    it('Should return 201 and outfit instance without image', async () => {
         const response = await request(app)
             .post('/api/outfits/')
             .set('Cookie', `token=${token}`)
-            .send({ clothes: clothesArray })
+            .send({ name: 'Outfit1', clothes: clothesArray })
             .expect(201);
         outfitID = response.body._id;
+    });
+    it('Should return 201 and clothes instance along with image link', async () => {
+        const response = await request(app)
+            .post('/api/outfits/')
+            .set('Cookie', `token=${token}`)
+            .set('Content-Type', 'multipart/form-data')
+            .field('name', nonExistingOutfit.name)
+            .field('clothes', String(clothesArray[0]._id))
+            .field('clothes', String(clothesArray[1]._id))
+            .attach('image', `${__dirname}/image.jpg`)
+            .expect(201);
+
+        expect(response.body).toEqual(
+            expect.objectContaining({
+                __v: 0,
+                _id: expect.any(String),
+                userID: expect.any(String),
+                cloudinaryID: expect.any(String),
+                favorited: expect.any(Boolean),
+                name: expect.any(String),
+                clothes: expect.any(Array),
+                createdAt: expect.any(String),
+                updatedAt: expect.any(String)
+            })
+        );
+        createdOutfitIdWithImage = response.body._id;
     });
 });
 describe('get all outfits', () => {
@@ -125,6 +127,71 @@ describe('get all favorited outfits', () => {
             .expect(200);
     });
 });
+
+describe('update an outfit', () => {
+    it('should return 200 and update the outfit', async () => {
+        await request(app)
+            .put(`/api/outfits/${outfitID}`)
+            .set('Cookie', `token=${token}`)
+            .send({ name: 'Outfit2', clothes: clothesArray })
+            .expect(200);
+    });
+    it('should return 404 if outfit is nonexisting', async () => {
+        await request(app)
+            .put(`/api/outfits/${nonExistingOutfitID}`)
+            .set('Cookie', `token=${token}`)
+            .send({ name: 'Outfit2', clothes: clothesArray })
+            .expect(404);
+    });
+    it('Should return 200 and updated clothes instance along with image link where there was not image initially', async () => {
+        const response = await request(app)
+            .put(`/api/outfits/${outfitID}`)
+            .set('Cookie', `token=${token}`)
+            .set('Content-Type', 'multipart/form-data')
+            .field('name', 'outfit3')
+            .attach('image', `${__dirname}/image.jpg`)
+            .expect(200);
+
+        expect(response.body).toEqual(
+            expect.objectContaining({
+                __v: 0,
+                _id: expect.any(String),
+                userID: expect.any(String),
+                cloudinaryID: expect.any(String),
+                favorited: expect.any(Boolean),
+                name: expect.any(String),
+                clothes: expect.any(Array),
+                createdAt: expect.any(String),
+                updatedAt: expect.any(String)
+            })
+        );
+        createdOutfitIdWithImage = response.body._id;
+    });
+    it('Should return 200 and updated clothes instance along with image link where there was image initially', async () => {
+        const response = await request(app)
+            .put(`/api/outfits/${createdOutfitIdWithImage}`)
+            .set('Cookie', `token=${token}`)
+            .set('Content-Type', 'multipart/form-data')
+            .field('name', 'outfit3')
+            .attach('image', `${__dirname}/image.jpg`)
+            .expect(200);
+
+        expect(response.body).toEqual(
+            expect.objectContaining({
+                __v: 0,
+                _id: expect.any(String),
+                userID: expect.any(String),
+                cloudinaryID: expect.any(String),
+                favorited: expect.any(Boolean),
+                name: expect.any(String),
+                clothes: expect.any(Array),
+                createdAt: expect.any(String),
+                updatedAt: expect.any(String)
+            })
+        );
+    });
+});
+
 describe('favoriting an outfit', () => {
     it('should not return 200 outfit is nonexisting', async () => {
         await request(app)
@@ -151,5 +218,20 @@ describe('unfavoriting an outfit', () => {
             .patch(`/api/outfits/unfavorite/${outfitID}`)
             .set('Cookie', `token=${token}`)
             .expect(200);
+    });
+});
+
+describe('delete an outfit', () => {
+    it('should return 200 and delete the outfit', async () => {
+        await request(app)
+            .delete(`/api/outfits/${outfitID}`)
+            .set('Cookie', `token=${token}`)
+            .expect(200);
+    });
+    it('should return 404 if outfit is nonexisting', async () => {
+        await request(app)
+            .delete(`/api/outfits/${nonExistingOutfitID}`)
+            .set('Cookie', `token=${token}`)
+            .expect(404);
     });
 });
