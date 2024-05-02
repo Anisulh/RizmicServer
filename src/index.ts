18528be5a879a484d2303921e9c119af468e5ee7
ubergoose <anisulhaque9391@gmail.com>
2024-05-02 00:41:59 -0400

    Add email service initialization in startServer function

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,13 @@ import { injectExitHandlerDependency } from './library/exitHandler';
 import logger from './library/logger';
 import { startApp } from './app';
 import dbConnection from './config/dbConnection';
+import emailService from './library/sendEmail';
 import './process';
 
 const startServer = async () => {
     await dbConnection();
     const app = await startApp();
+    await emailService.init();
     const server = app.listen(config.port, () => {
         logger.info(`Server is running on port: ${config.port}`);
     });
