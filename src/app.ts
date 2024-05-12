7c3769fb2faf39aaa750f1a8714209894090d5b0
ubergoose <anisulhaque9391@gmail.com>
2024-05-12 17:43:27 -0400

    RFS-7 add friendsRouter to handle API requests for friends

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,7 @@ import outfitRouter from './components/outfits/route';
 import Rollbar from 'rollbar';
 import cookieParser from 'cookie-parser';
 import handleError from './middleware/handleError';
+import friendsRouter from './components/friends/route';
 
 
 export const rollbar = new Rollbar({
@@ -43,6 +44,7 @@ export const startApp = async (): Promise<Application> => {
         res.status(200).send('OK');
     });
     app.use('/api/user', userRouter);
+    app.use('/api/friends', friendsRouter)
     app.use('/api/clothes', clothesRouter);
     app.use('/api/generation', generationRouter);
     app.use('/api/outfits', outfitRouter);
