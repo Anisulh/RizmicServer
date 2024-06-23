3c7ee77b8b0e42bf73a9636e199918424c454ecb
ubergoose <anisulhaque9391@gmail.com>
2024-06-23 10:51:10 -0400

    add email field to getFriends response and prettier formatting

--- src/components/friends/controller.ts
+++ src/components/friends/controller.ts
@@ -1,4 +1,3 @@
-
 import { Request, Response } from 'express';
 import Friends from './model';
 import { AppError } from '../../library/errorHandler';
@@ -8,6 +7,7 @@ interface IUser {
     firstName: string;
     lastName: string;
     profilePicture: string;
+    email: string;
 }
 
 interface IFriendship {
@@ -17,43 +17,49 @@ interface IFriendship {
 }
 
 export async function getFriends(req: Request, res: Response) {
-  const { _id } = req.user;
-  const friendships = await Friends.find({
-    $or: [{ requester: _id }, { recipient: _id }],
-    status: 'accepted',
-  }).populate('requester recipient', 'firstName lastName profilePicture').lean<IFriendship[]>();
-  // Map over each friendship to return only the friend's data
-  const friends = friendships.map(friendship => {
-    // Check if the requester is the current user, if so return the recipient
-    if (friendship.requester._id.toString() === _id.toString()) {
-        return {
-            _id: friendship.recipient._id,
-            firstName: friendship.recipient.firstName,
-            lastName: friendship.recipient.lastName,
-            profilePicture: friendship.recipient.profilePicture
-        };
-    } else {
-        // Otherwise, return the requester
-        return {
-            _id: friendship.requester._id,
-            firstName: friendship.requester.firstName,
-            lastName: friendship.requester.lastName,
-            profilePicture: friendship.requester.profilePicture
-        };
-    }
-});
+    const { _id } = req.user;
+    const friendships = await Friends.find({
+        $or: [{ requester: _id }, { recipient: _id }],
+        status: 'accepted'
+    })
+        .populate('requester recipient', 'firstName lastName profilePicture')
+        .lean<IFriendship[]>();
+    // Map over each friendship to return only the friend's data
+    const friends = friendships.map((friendship) => {
+        // Check if the requester is the current user, if so return the recipient
+        if (friendship.requester._id.toString() === _id.toString()) {
+            return {
+                _id: friendship.recipient._id,
+                firstName: friendship.recipient.firstName,
+                lastName: friendship.recipient.lastName,
+                email: friendship.recipient.email,
+                profilePicture: friendship.recipient.profilePicture
+            };
+        } else {
+            // Otherwise, return the requester
+            return {
+                _id: friendship.requester._id,
+                firstName: friendship.requester.firstName,
+                lastName: friendship.requester.lastName,
+                email: friendship.requester.email,
+                profilePicture: friendship.requester.profilePicture
+            };
+        }
+    });
 
-  res.status(200).json(friends);
+    res.status(200).json(friends);
 }
 
 export async function getFriendRequests(req: Request, res: Response) {
     const { _id } = req.user;
     const friendRequests = await Friends.find({
         recipient: _id,
-        status: 'pending',
-    }).populate('requester', 'firstName lastName email profilePicture').lean();
+        status: 'pending'
+    })
+        .populate('requester', 'firstName lastName email profilePicture')
+        .lean();
 
-    res.status(200).json(friendRequests );
+    res.status(200).json(friendRequests);
 }
 
 export async function sendFriendRequest(req: Request, res: Response) {
@@ -61,18 +67,24 @@ export async function sendFriendRequest(req: Request, res: Response) {
     const { _id } = req.user;
 
     if (_id === userId) {
-       throw new AppError({ message: 'You cannot send a friend request to yourself', httpCode: 400 });
+        throw new AppError({
+            message: 'You cannot send a friend request to yourself',
+            httpCode: 400
+        });
     }
 
     const friendRequest = await Friends.findOne({
         $or: [
             { requester: _id, recipient: userId },
-            { requester: userId, recipient: _id },
-        ],
+            { requester: userId, recipient: _id }
+        ]
     });
 
     if (friendRequest) {
-        throw new AppError({ message: 'Friend request already sent', httpCode: 400 });
+        throw new AppError({
+            message: 'Friend request already sent',
+            httpCode: 400
+        });
     }
 
     await Friends.create({ requester: _id, recipient: userId });
@@ -81,7 +93,6 @@ export async function sendFriendRequest(req: Request, res: Response) {
 }
 
 export async function acceptFriendRequest(req: Request, res: Response) {
-   
     const { userId } = req.params;
     const { _id } = req.user;
 
@@ -91,27 +102,33 @@ export async function acceptFriendRequest(req: Request, res: Response) {
     );
 
     if (!friendRequest) {
-        throw new AppError({ message: 'Friend request not found', httpCode: 404 });
+        throw new AppError({
+            message: 'Friend request not found',
+            httpCode: 404
+        });
     }
 
     res.status(200).json({ message: 'Friend request accepted' });
 }
 
 export async function unfriendUser(req: Request, res: Response) {
-    
     const { userId } = req.params;
     const { _id } = req.user;
 
     const friendRequest = await Friends.findOneAndDelete({
         $or: [
             { requester: _id, recipient: userId },
-            { requester: userId, recipient: _id },
-        ],
+            { requester: userId, recipient: _id }
+        ]
     });
 
     if (!friendRequest) {
-        throw new AppError({ message: 'Friend request not found', httpCode: 404 });
+        throw new AppError({
+            message: 'Friend request not found',
+            httpCode: 404
+        });
     }
 
     res.status(200).json({ message: 'Friend removed' });
 }
+
