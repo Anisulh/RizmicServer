93f2d9942f3ac6ed7df77634ae66689fc350e89e
ubergoose <anisulhaque9391@gmail.com>
2024-06-25 10:46:26 -0400

    add timestamps

--- src/components/friends/model.ts
+++ src/components/friends/model.ts
@@ -2,15 +2,18 @@ import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
 
-const friendsSchema = new Schema({
-    requester: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    recipient: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    status: {
-        type: String,
-        enum: ['pending', 'accepted', 'declined'],
-        default: 'pending'
-    }
-});
+const friendsSchema = new Schema(
+    {
+        requester: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+        recipient: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+        status: {
+            type: String,
+            enum: ['pending', 'accepted', 'declined'],
+            default: 'pending'
+        }
+    },
+    { timestamps: true }
+);
 
 const Friends = mongoose.model('Friends', friendsSchema);
 
