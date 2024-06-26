cd177b2edd7cd0ed83769140750adf5a84e36dd5
ubergoose <anisulhaque9391@gmail.com>
2024-06-26 09:25:55 -0400

    add newToken and tokenExpiry to request type

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,6 +1,8 @@
 declare namespace Express {
     export interface Request {
         user: any;
+        newToken: boolean;
+        tokenExpiry: number;
     }
     export interface Response {
         user: any;
