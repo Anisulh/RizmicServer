5127272188f94b50d552d4f0c2cc8dc63d0334d0
ubergoose <anisulhaque9391@gmail.com>
2024-07-01 18:23:58 -0400

    add share outfits schema

--- src/components/friends/validationSchema.ts
+++ src/components/friends/validationSchema.ts
@@ -3,3 +3,7 @@ import { z } from 'zod';
 export const shareClothesSchema = z.object({
     friends: z.array(z.string()).min(1)
 });
+
+export const shareOutfitsSchema = z.object({
+    friends: z.array(z.string()).min(1)
+});
