6d3052ae91a7316f093e5ac599a7fa0f3bca95fa
ubergoose <anisulhaque9391@gmail.com>
2024-07-01 11:48:23 -0400

    add new routes to share clothing and get friend profile

--- src/components/friends/route.ts
+++ src/components/friends/route.ts
@@ -4,15 +4,24 @@ import {
     getFriendRequests,
     sendFriendRequest,
     acceptFriendRequest,
-    unfriendUser
+    unfriendUser,
+    shareClothing,
+    getFriendProfile
 } from './controller';
 import { authorization } from '../../middleware/authorization';
 import asyncHandler from 'express-async-handler';
+import { shareClothesSchema } from './validationSchema';
+import { reqValidation } from '../../middleware/reqValidation';
 
 const friendsRouter = Router();
 
 friendsRouter.get('/', authorization, asyncHandler(getFriends));
 friendsRouter.get('/requests', authorization, asyncHandler(getFriendRequests));
+friendsRouter.get(
+    '/profile/:userId',
+    authorization,
+    asyncHandler(getFriendProfile)
+);
 friendsRouter.post(
     '/request/:userId',
     authorization,
@@ -27,6 +36,12 @@ friendsRouter.delete(
     '/unfriend/:userId',
     authorization,
     asyncHandler(unfriendUser)
+);
+friendsRouter.post(
+    '/share/clothing/:clothesId',
+    authorization,
+    reqValidation(shareClothesSchema),
+    asyncHandler(shareClothing)
 );
 
 export default friendsRouter;
